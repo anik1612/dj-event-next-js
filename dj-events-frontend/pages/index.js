5d8b6f894d18eaec8ae21d2f9e9f9327f37a1f9d
Anik <aniksarker1612@gmail.com>
2021-09-20 15:15:04 +0800

    main structure is ready & data is coming from data.json

--- dj-events-frontend/pages/index.js
+++ dj-events-frontend/pages/index.js
@@ -1,15 +1,21 @@
-import Layout from '../components/Layout';
-import Head from 'next/head';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+	console.log(events);
 	return (
-		<Layout>
-			<Head>
-				<title>DJ Events</title>
-				<meta name='description' content='Welcome to DJ Events' />
-			</Head>
-      
-			<h1>Home</h1>
+		<Layout title='DJ Events' description='Welcome to DJ Events'>
+			<h1>Upcoming Events</h1>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+
+	return {
+		props: { events },
+		revalidate: 1,
+	};
 }
